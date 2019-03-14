6935b0c8ba3b9d849f8e453b1c4d72f9a612756a
Mathys Taquet <mtaquet@z2r7p6.le-101.fr>
2019-03-14 14:48:50 +0100

    resol

--- srcs/resol.c
+++ srcs/resol.c
@@ -6,10 +6,17 @@
 /*   By: mtaquet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/03/14 14:26:27 by mtaquet      #+#   ##    ##    #+#       */
-/*   Updated: 2019/03/14 14:26:38 by mtaquet     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/03/14 14:48:13 by mtaquet     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
+int	put_moving_ant(t_map *map, int ant, int room)
+{
+	ft_putchar('L');
+	ft_putnbr(ant + 1);
+	ft_putchar('-');
+	ft_putstr(map->room[room].name);
+}
