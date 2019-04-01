044a6969c7d2c9a6c8e8ba304a933e7fc14f517f
Lucas Perron <lperron@z2r6p5.le-101.fr>
2019-04-01 15:02:19 +0200

    It worked for me...

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: mtaquet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/02/21 14:09:06 by mtaquet      #+#   ##    ##    #+#       */
-/*   Updated: 2019/04/01 14:58:20 by lperron     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/04/01 15:01:35 by lperron     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -89,6 +89,6 @@ int						gogogo(t_map *map, int *count);
 void					put_resol(t_map *map, int ant, int room);
 void					sort_best_comp(t_map *map);
 int						how_long_will_it_be(t_map *map, int min_path,
-						int plusant)
+						int plusant, int *path);
 
 #endif
--- srcs/move_ant.c
+++ srcs/move_ant.c
@@ -6,7 +6,7 @@
 /*   By: lperron <lperron@student.le-101.f>         +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/03/29 12:13:30 by lperron      #+#   ##    ##    #+#       */
-/*   Updated: 2019/04/01 14:23:02 by lperron     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/04/01 15:01:47 by lperron     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -22,14 +22,14 @@ void	continue_path(t_map *map, int j, int *arrived)
 	put_resol(map, j, map->ant[j].room);
 }
 
-int		how_long_will_it_be(t_map *map, int min_path, int plusant)
+int		how_long_will_it_be(t_map *map, int min_path, int plusant, int	*path)
 {
 	int	count;
 	int	arrived;
 	int	i;
 	int incr;
 
-	count = map->path_len[map->best_compa[0]] - 1;
+	count = map->path_len[path[0]] - 1;
 	arrived = 0;
 	incr = 0;
 	while (arrived < map->nb_ant + plusant)
@@ -38,7 +38,7 @@ int		how_long_will_it_be(t_map *map, int min_path, int plusant)
 		i = incr - 1;
 		while (++i <= incr && i <= min_path)
 		{
-			if (count >= map->path_len[map->best_compa[i]])
+			if (count >= map->path_len[path[i]])
 				incr++;
 			else
 				break ;
@@ -78,8 +78,9 @@ void	can_i_go(t_map *map, int j, int *arrived, int ant)
 	min_path = get_min_path(map, ant);
 	if (map->best_compa[min_path] != -1)
 	{
-		if (min_path == 0 || (how_long_will_it_be(map, min_path - 1, min_path)
-					> how_long_will_it_be(map, min_path, min_path)))
+		if (min_path == 0 || (how_long_will_it_be(map, min_path - 1, min_path,
+		map->best_compa) > how_long_will_it_be(map,
+		min_path, min_path, map->best_compa)))
 		{
 			map->nb_ant--;
 			map->ant[j].i_path = 0;
